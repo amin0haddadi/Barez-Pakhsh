f9c736e95cb13515a5034580ac3f4fde4aa81cc8
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-12-25 21:17:38 +0330

    refactor: add neccery types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,38 @@ export interface Post {
   title: string;
   body: string;
 }
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface CenteredListItemProps {
+  text: string;
+  key: number;
+}
