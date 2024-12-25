4fe6ebb3a0df5de9b52e8a057f5c3d461c940c28
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-12-25 21:20:29 +0330

    refactor: add login simulation and user authentication to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,45 @@
 "use client";
 import { HEADER_ITEMS } from "@/constants";
+import { getRandomUser } from "@/utils/getRandomUser";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const path = usePathname();
 
+  const [isEligible, setIsEligible] = useState(false);
+
+  useEffect(() => {
+    getRandomUser();
+
+    const userCookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("auth_user"))
+      ?.split("=")[1];
+
+    if (userCookie) {
+      const user = JSON.parse(decodeURIComponent(userCookie));
+      console.log(user.id);
+      if (user?.id <= 5) {
+        setIsEligible(true);
+      }
+    }
+  }, []);
+
+  const menuItems = isEligible
+    ? HEADER_ITEMS.filter((item) => item !== "setting")
+    : HEADER_ITEMS;
+
   return (
-    <header className="flex flex-row justify-center w-[100%] bg-sky-400 p-5 gap-4">
-      {HEADER_ITEMS.map((item: string, index: number) => (
+    <header className="flex flex-row justify-center w-[100%] bg-sky-300 p-5 gap-4">
+      {menuItems.map((item: string, index: number) => (
         <Link key={index} href={`/${item}`}>
           <h3
             className={`text-xl text-white  ${
               path === "/" + item
-                ? "text-blue-800 scale-110"
-                : "hover:text-blue-500 hover:scale-105"
+                ? "text-indigo-800 scale-110"
+                : "hover:text-blue-400 hover:scale-105"
             }`}
           >
             {item}
