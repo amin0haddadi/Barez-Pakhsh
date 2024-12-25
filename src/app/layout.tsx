99cc4d61f656a2707d1fa90fcacf522f2b8cdbfb
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-12-25 17:35:20 +0330

    feature: implement layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
+import { rootMetadata } from "@/config/metadata";
 import "./globals.css";
+import { ReactQueryProvider } from "@/providers";
+import { PublicLayout } from "@/public/PublicLayout";
 
-export const metadata: Metadata = {
-  title: "PSP Project",
-  description: "exam test",
-};
+export const metadata = rootMetadata;
 
 type RootLayoutProps = Readonly<{
   children: React.ReactNode;
@@ -13,8 +12,10 @@ type RootLayoutProps = Readonly<{
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className={`w-screen h-screen flex flex-col overflow-x-hidden`}>
+        <ReactQueryProvider>
+          <PublicLayout>{children}</PublicLayout>
+        </ReactQueryProvider>
       </body>
     </html>
   );
---
+++ src/config/metadata.ts
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+const rootMetadata: Metadata = {
+  title: {
+    template: "%s - PSP",
+    default: "شرکت PSP",
+  },
+  description: "شرکت حمل و نقل بین المللی PSP",
+  authors: [
+    {
+      name: "Amin Haddadi",
+    },
+  ],
+  keywords: [],
+};
+
+export { rootMetadata };
