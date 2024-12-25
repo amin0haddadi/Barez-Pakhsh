bad4918d4cb31b4c323fa3494a77befaa0fa374d
Amin Haddadi <amin.haddadi1995@gmail.com>
2024-12-25 21:36:39 +0330

    fix: change style of li

--- src/components/CenteredListItem/index.tsx
+++ src/components/CenteredListItem/index.tsx
@@ -6,7 +6,7 @@ const CenteredListItem: FC<CenteredListItemProps> = ({ text, key }) => {
     <div className="flex justify-center items-center">
       <li
         key={key}
-        className="bg-gray-300 w-[40%] p-10 rounded-lg shadow-lg transform transition-transform duration-200 hover:scale-105"
+        className="bg-gray-300 w-[40%] p-10 rounded-lg shadow-lg opacity-60 transform transition-transform duration-200 hover:scale-105 hover:opacity-100"
       >
         <p className="text-center text-xl">{text}</p>
       </li>
